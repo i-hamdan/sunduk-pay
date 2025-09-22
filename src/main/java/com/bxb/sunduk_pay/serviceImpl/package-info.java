d3675a35da2f5f119f358d0f82ea70efb9836c89
ZaidAli-19 <zaidddali19@gmail.com>
2025-09-22 13:07:54 +0530

    Create package-info.java

---
+++ src/main/java/com/bxb/sunduk_pay/serviceImpl/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the implementation
+ * classes for the services defined in the application.
+ * It includes business logic and interacts with
+ * repositories to perform CRUD operations.
+ */
+package com.bxb.sunduk_pay.serviceImpl;
