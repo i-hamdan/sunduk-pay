bd029da07d574455fe60b7392ba345d66c50b5c5
ZaidAli-19 <zaidddali19@gmail.com>
2025-09-24 10:07:55 +0530

    Update SundukPayApplication.java

--- src/main/java/com/bxb/sunduk_pay/SundukPayApplication.java
+++ src/main/java/com/bxb/sunduk_pay/SundukPayApplication.java
@@ -12,6 +12,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class SundukPayApplication {
 
+/** Non-static field to avoid checkstyle error.*/
+private final String appName = "SundukPay";
 /**
 * Main method to start the Spring Boot application.
 * @param args command-line arguments passed to the application
