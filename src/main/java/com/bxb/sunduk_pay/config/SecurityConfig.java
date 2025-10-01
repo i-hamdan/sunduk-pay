f09be0bc5605a00390fc6c5dd0db4f06e42cd943
tayyabk121 <tayyabkhanbhopal@gmail.com>
2025-10-01 14:22:18 +0530

    Update SecurityConfig.java

--- src/main/java/com/bxb/sunduk_pay/config/SecurityConfig.java
+++ src/main/java/com/bxb/sunduk_pay/config/SecurityConfig.java
@@ -9,15 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.session.web.http.CookieSerializer;
-import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 /**
  * Spring Security configuration for the application.
@@ -47,61 +40,65 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/custom-login").authenticated()
-                        .anyRequest().permitAll())
-                .oauth2Login(auth -> auth.defaultSuccessUrl("/custom-login"))
+                        .anyRequest().permitAll()
+                );
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                        .requestMatchers("/custom-login").authenticated()
+//                        .anyRequest().permitAll())
+//                .oauth2Login(auth -> auth.defaultSuccessUrl("/custom-login"))
                 //.oauth2Login(Customizer.withDefaults())
-                .sessionManagement(session -> session
-                        .maximumSessions(1)
-                        .maxSessionsPreventsLogin(true))
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .invalidateHttpSession(true));
+//                .sessionManagement(session -> session
+//                        .maximumSessions(1)
+//                        .maxSessionsPreventsLogin(true))
+//                .logout(logout -> logout
+//                        .logoutUrl("/logout")
+//                        .invalidateHttpSession(true));
 //                .addFilterBefore(filter,
 //                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
-    /**
-     * Publishes session lifecycle events so Spring Security’s concurrent
-     * session control works.
-     *
-     * @return {@link HttpSessionEventPublisher} bean
-     */
+//    /**
+//     * Publishes session lifecycle events so Spring Security’s concurrent
+//     * session control works.
+//     *
+//     * @return {@link HttpSessionEventPublisher} bean
+//     */
+//
+//    @Bean
+//    public HttpSessionEventPublisher httpSessionEventPublisher() {
+//        return new HttpSessionEventPublisher();
+//    }
+//
+//    /**
+//     * Configure allowed origins/headers/methods for cross-origin requests.
+//     *
+//     * @return {@link CorsConfigurationSource} with allowed settings
+//     */
 
-    @Bean
-    public HttpSessionEventPublisher httpSessionEventPublisher() {
-        return new HttpSessionEventPublisher();
-    }
 
-    /**
-     * Configure allowed origins/headers/methods for cross-origin requests.
-     *
-     * @return {@link CorsConfigurationSource} with allowed settings
-     */
-
-
-    @Bean
-    /* we will be not needing this method once moved to domain**/
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of(
-                "http://localhost:5174",
-                "https://f6be298fe7d5.ngrok-free.app",
-                "http://localhost:5173"
-
-        ));
-        corsConfiguration.setAllowedMethods(
-                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedHeaders(List.of("*"));
-        UrlBasedCorsConfigurationSource source
-                = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration(
-                "/**", corsConfiguration);
-        return source;
-    }
+//    @Bean
+//    /* we will be not needing this method once moved to domain**/
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.setAllowedOrigins(List.of(
+//                "http://localhost:5174",
+//                "https://f6be298fe7d5.ngrok-free.app",
+//                "http://localhost:5173"
+//
+//        ));
+//        corsConfiguration.setAllowedMethods(
+//                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        corsConfiguration.setAllowCredentials(true);
+//        corsConfiguration.setAllowedHeaders(List.of("*"));
+//        UrlBasedCorsConfigurationSource source
+//                = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration(
+//                "/**", corsConfiguration);
+//        return source;
+//    }
 
     /**
      * Customize session cookie behaviour.
@@ -109,14 +106,14 @@ public class SecurityConfig {
      * @return {@link CookieSerializer} with configured settings
      */
 
-    @Bean
-    public CookieSerializer cookieSerializer() {
-        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-        serializer.setSameSite("None");
-        serializer.setUseSecureCookie(true); // if using https
-        serializer.setCookieName("JSESSIONID");
-        return serializer;
-    }
+//    @Bean
+//    public CookieSerializer cookieSerializer() {
+//        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+//        serializer.setSameSite("None");
+//        serializer.setUseSecureCookie(true); // if using https
+//        serializer.setCookieName("JSESSIONID");
+//        return serializer;
+//    }
 
     /**
      * Provides a RestTemplate bean for making REST API calls.
