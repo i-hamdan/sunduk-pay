3ada080c6ec44f8add3cd018febb94d94ae26820
tayyabk121 <tayyabkhanbhopal@gmail.com>
2025-10-01 14:13:29 +0530

    Update AuthenticationFilter.java

--- src/main/java/com/bxb/sunduk_pay/config/AuthenticationFilter.java
+++ src/main/java/com/bxb/sunduk_pay/config/AuthenticationFilter.java
@@ -1,14 +1,8 @@
 package com.bxb.sunduk_pay.config;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
@@ -16,7 +10,7 @@ import java.io.IOException;
  * Filter for authenticating requests by validating session and cookies.
  */
 @Log4j2
-@Component
+//@Component
 @RequiredArgsConstructor
 public class AuthenticationFilter implements Filter {
 //  /** Security properties containing excluded
