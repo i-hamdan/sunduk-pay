c8c52b831513c133a56a5ccc99dedeb231bdcae2
ZaidAli-19 <zaidddali19@gmail.com>
2025-10-22 12:30:50 +0530

    another fix

--- src/main/java/com/bxb/sunduk_pay/SundukPayApplication.java
+++ src/main/java/com/bxb/sunduk_pay/SundukPayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @SpringBootApplication
 @EnableScheduling
+@EnableKafka
 public class SundukPayApplication {
 
 /**Non-static application name to avoid checkstyle error.*/
