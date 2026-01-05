d0f9db12864127184a99ac88fd15709133ed32f2
MdBilal39 <bilal81038@gmail.com>
2026-01-05 09:58:23 +0530

    added admin field on global pot

--- src/test/java/com/bxb/sunduk_pay/factories/InvestmentFactory/CancelInvestmentServiceTest.java
+++ src/test/java/com/bxb/sunduk_pay/factories/InvestmentFactory/CancelInvestmentServiceTest.java
@@ -26,7 +26,7 @@
 //import static org.mockito.Mockito.when;
 //
 //@ExtendWith(MockitoExtension.class)
-//class CancelInvestmentServiceTest {
+//claspers CancelInvestmentServiceTest {
 //
 //    @Mock
 //    private InvestmentRepository investmentRepository;
