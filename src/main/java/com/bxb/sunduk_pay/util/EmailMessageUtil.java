af3b2aa736fc868b054d10be744f750bdc6d16cf
kaushar-mecaps <akaushar690@gmail.com>
2026-01-16 11:10:46 +0530

    update email util

--- src/main/java/com/bxb/sunduk_pay/util/EmailMessageUtil.java
+++ src/main/java/com/bxb/sunduk_pay/util/EmailMessageUtil.java
@@ -107,9 +107,11 @@ public final class EmailMessageUtil {
                  <meta name="viewport"
                   content="width=device-width, initial-scale=1.0" />
                  <title>4 Digit PIN To Reset Sunduk MPIN</title>
-                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+<link href=
+"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
                  rel="stylesheet" />
-                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"\s
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;
+700&display=swap"\s
                  rel="stylesheet" />
                  <style>
                  * {font-family: "Poppins", sans-serif !important;}
