83c6ad45d6549f731a0ecffa7de27f65479d9391
ZaidAli-19 <zaidddali19@gmail.com>
2026-01-28 11:37:20 +0530

    configuration fix

--- src/main/java/com/bxb/sunduk_pay/config/MySQLConfig.java
+++ src/main/java/com/bxb/sunduk_pay/config/MySQLConfig.java
@@ -30,9 +30,9 @@ public class MySQLConfig {
     public DataSource mysqlDataSource() {
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/SundukPay");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root123");
+        dataSource.setJdbcUrl("jdbc:mysql://walletapp-mysql:3306/sundukpay");
+        dataSource.setUsername("walletuser");
+        dataSource.setPassword("walletpass123");
         dataSource.setMaximumPoolSize(10);
         return dataSource;
     }
