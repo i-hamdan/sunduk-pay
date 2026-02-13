40378710a7e817a932dd025c7e23ea56a46c26c4
Mohsin8705 <mohsinmohd8705@gmail.com>
2026-02-13 13:57:06 +0530

    Update MpinEncryption.java

--- src/main/java/com/bxb/sunduk_pay/encryption/MpinEncryption.java
+++ src/main/java/com/bxb/sunduk_pay/encryption/MpinEncryption.java
@@ -12,7 +12,7 @@ public class MpinEncryption {
     /**
      * Cost factor for BCrypt hashing.
      */
-    private static final int COST_FACTOR = 12;
+    private static final int COST_FACTOR = 6;
 
     /**
      * Encrypts the given MPIN using BCrypt hashing.
