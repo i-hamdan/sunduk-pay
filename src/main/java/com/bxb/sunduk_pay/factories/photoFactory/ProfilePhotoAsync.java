842543a00ab2811a466f64e2867cf48299d8efd7
Yousuf Mohammed Khan <yousufmkhan333@gmail.com>
2026-02-24 13:12:52 +0530

    profile upload threadpool increased

--- src/main/java/com/bxb/sunduk_pay/factories/photoFactory/ProfilePhotoAsync.java
+++ src/main/java/com/bxb/sunduk_pay/factories/photoFactory/ProfilePhotoAsync.java
@@ -8,14 +8,9 @@ import com.bxb.sunduk_pay.validations.Validations;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * ProfilePhotoAsync handles the asynchronous uploading of profile photos.
@@ -33,7 +28,13 @@ public class ProfilePhotoAsync {
 
     /**     * Executor for handling asynchronous photo upload tasks. */
     private final ExecutorService photoExecutor =
-            Executors.newFixedThreadPool(2);
+            new ThreadPoolExecutor(
+                    8,
+                    16,
+                    60,
+                    TimeUnit.SECONDS,
+                    new LinkedBlockingQueue<>(1000)
+            );
 
 
     /**     * Uploads the profile photo in the background.
